a6253ba1c36a91984a0ad13dff60b1e1517fbf6e
Alvin <asunayuuki289@gmail.com>
2019-10-02 13:16:38 +0700

    Tugas3

--- Alvin/html.php
+++ Alvin/html.php
@@ -8,10 +8,45 @@
         <center>
         
         <?php
-        echo '<img src="Image.jpeg" width ="350px" height = "200px">
-        <h1 style = "color :red">Selamat Belajar Html!</h1>
-        <h2 style = "color :aqua">Nama Saya : Alvin</h2>
-        <h3>Politeknik Negeri Jember</h3>';
-        ?>
+    echo "Hallowww Kelas </br></br>";
+    $tempat = "di Polije";
+	$Tempat = "di Polije Dua";
+	$TempaT = "di Polije Tiga";
+
+	echo "<h1 style='color:red'>Selamat Belajar HTML ".$tempat."</h1>";
+	ECHO "<h2 style='color:blue'>Selamat Belajar HTML</h2>";
+	EcHo "<h3>Selamat Belajar HTML ".$Tempat."</h3>";
+	echo $TempaT."</br></br>";
+	echo date("Y/m/d")."</br></br>";
+
+	//Penjumlahan
+	$a = 10; $b = $c = 20; $d = 2;
+	$jumlah = ($a + $b)/$d;
+
+	echo $jumlah."</br></br>";
+
+	for ($x = 0; $x <= 10; $x++) {
+    	echo "Tempat Ke - ".$x."<br>";
+		}
+
+	$t = date("H");
+
+	if ($t < "20") {
+    	echo "Selamat Pagi!";
+	}
+
+	//Konstanta
+	define("Alvin", "Selamat datang di Polije!");
+	echo "</br></br>".Alvin;
+
+	//Fungsi
+	function Alvin(){
+		echo "</br></br>".Alvin." Dua";
+	}
+
+	Alvin();
+
+?>
+
     </body>
 </html>
