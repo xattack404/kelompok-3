baa0ba73651f9811327687268690130af8b85532
yudistiono <yudistiono992@gmail.com>
2019-10-02 08:20:12 +0700

    ganti file html ke php

--- yudis/tugas1.php
+++ yudis/tugas1.php
@@ -23,6 +23,17 @@
             </ul>
 
         </form>
+        <?php 
+
+        echo '
+        <center>
+            <h1 style = "color :green">Selamat Belajar Html!</h1>
+            <h2 style = "color :aqua">Nama Saya : Yudistiono</h2>
+            <h3>Politeknik Negeri Jember</h3>
+        </center>
+        ';
+
+         ?>
     </body>
 
 </html>
