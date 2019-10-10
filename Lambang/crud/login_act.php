cf7daa10dabdd0e4393c1016293281b53f24b6af
E41181803 - Lambang Arinanda Hadi <55471760+xattack404@users.noreply.github.com>
2019-10-10 21:24:43 +0700

    jiji

--- Lambang/crud/login_act.php
+++ Lambang/crud/login_act.php
@@ -10,7 +10,7 @@ $username = $_POST['username'];
 $password = md5($_POST['password']);
  
 // menyeleksi data admin dengan username dan password yang sesuai
-$data = mysqli_query($koneksi,"select * from admin where username='$username' and password='$password'");
+$data = mysqli_query($koneksi,"select * from login where username='$username' and password='$password'");
  
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($data);
@@ -18,7 +18,7 @@ $cek = mysqli_num_rows($data);
 if($cek > 0){
 	$_SESSION['username'] = $username;
 	$_SESSION['status'] = "login";
-	header("location:admin/index.php");
+	header("location:view.php");
 }else{
 	header("location:index.php?pesan=gagal");
 }
