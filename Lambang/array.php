794a45de8b04c92078642efae552f063518463ea
E41181803 - Lambang Arinanda Hadi <55471760+xattack404@users.noreply.github.com>
2019-10-11 09:31:40 +0700

    a

---
+++ Lambang/array.php
@@ -0,0 +1,10 @@
+<?php 
+    //membuat array yang berisi nama buah-buahan
+    $buah = array('semangka','jeruk','apel','anggur');
+
+    //count() untuk menghitung isi array.
+    for($i=0;$i<count($buah);$i++){
+	echo $buah [$i]."<br/>";
+}
+ 
+?>
