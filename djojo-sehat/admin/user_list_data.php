e74158c2ccfaa32996096a7054013cc413608a67
yudistiono <yudistiono992@gmail.com>
2019-10-16 00:59:59 +0700

    sementara untuk tampil data user
    nah untuk semua id baik di tabel produk maupun di tabel lain rencananya pakai uuid jadi field untuk id yang panjangnya 15 diubah menjadi minimal lah 50
    nah yang ini cuma untuk menampilkan data user atau read doang untuk tambah dan update proses masih

--- djojo-sehat/admin/user_list_data.php
+++ djojo-sehat/admin/user_list_data.php
@@ -6,45 +6,41 @@
           <th style="text-align: center">No.</th>
           <th style="text-align: center">Nama</th>
           <th style="text-align: center">Username</th>
-          <th style="text-align: center">Tipe User</th>
-          <th style="text-align: center">Jam Buat</th>
-          <th style="text-align: center">Tgl Buat</th>
+          <th style="text-align: center">No Telp</th>
+          <th style="text-align: center">Posisi</th>
+          <!-- <th style="text-align: center"></th> -->
           <th style="text-align: center">Aksi</th>
         </tr>
       </thead>
       <tbody>
 
       <?php
-      $sql = "SELECT * FROM user WHERE id_user > 0 ORDER BY id_user DESC";
-      $result = mysqli_query($conn, $sql);
+      $sql = "SELECT *, tb_login.id_posisi as no FROM tb_login, tb_posisi WHERE  tb_login.id_posisi = tb_posisi.id_posisi ORDER BY id_login DESC";
+      $result = mysqli_query($koneksi, $sql);
       $no = 1;
       if (mysqli_num_rows($result) > 0)
       {
         while ($data = mysqli_fetch_array($result))
-        {
-          echo "<tr>
-                  <td style='text-align: center'>".$no."</td>
-                  <td style='text-align: left'>".$data['nama']."</td>
-                  <td style='text-align: center'>".$data['username']."</td>
-                  <td style='text-align: center'>".$data['usertype']."</td>
-                  <td style='text-align: center'>".$data['jam_upload']."</td>
-                  <td style='text-align: center'>".tgl_indo($data['tgl_upload'])."</td>
-                  <td style='text-align: center'>
-                    <a href='user_ubah.php?id_user=".$data['id_user']."' '>
-                      <button type='submit' class='btn btn-primary'>Ubah</button>
+        {?>
+          <tr>
+                  <td style="text-align: center"><?= $no++ ?></td>
+                  <td style="text-align: left"><?= $data['nama'] ?></td>
+                  <td style="text-align: center"><?= $data['username'] ?></td>
+                  <td style="text-align: center"><?= $data['no_hp'] ?></td>
+                  <td style="text-align: center"><?= $data['posisi'] ?></td>
+                  <td style="text-align: center">
+                    <a href="user_ubah.php?id_user=$data['id_user']">
+                      <button type="submit" class="btn btn-primary">Ubah</button>
                     </a>
-                    <a href='user_hapus.php?id_user=".$data['id_user']."' '>
-                      <button type='submit' class='btn btn-danger' OnClick=\"return confirm('Apakah Anda yakin?');\">Hapus</button>
+                    <a href="user_hapus.php?id_user=">
+                      <button type="submit" class="btn btn-danger" OnClick="return confirm('Apakah Anda yakin?');">Hapus</button>
                     </a>
                   </td>
-                </tr>";
-                $no++;
+          </tr>
+              
+       <?php
         }
       }
-        else
-        {
-          echo "Belum ada data";
-        }
       ?>
       </tbody>
   </table>
