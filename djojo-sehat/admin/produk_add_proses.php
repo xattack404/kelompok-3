e04b3e359a4686353444f87e86ec1f2d11d53785
E41181803 - Lambang Arinanda Hadi <55471760+xattack404@users.noreply.github.com>
2019-10-27 16:11:50 +0700

    tgs

--- djojo-sehat/admin/produk_add_proses.php
+++ djojo-sehat/admin/produk_add_proses.php
@@ -4,7 +4,7 @@ include 'cek_login.php';        // Panggil fungsi cek sudah login/belum
 include 'cek_session.php';      // Panggil fungsi cek session
 include '../fungsi/cek_aksi_tambah.php';  // Panggil fungsi boleh tamabah data atau tidak
 include '../fungsi/judul_seo.php';        // Panggil fungsi judul_seo untuk merubah teks dalam format tanpa spasi dan simbol
-require "template/libs/vendor/autoload.php";
+require "template/libs/vendor/autoload.php"; //Panggil Composser
 
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\Exception\UnsatisfiedDepedencyException;
