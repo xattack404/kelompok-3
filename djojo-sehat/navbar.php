307b7a8687d3f931660b893147e7a8f4a5ef09f6
Alvin <asunayuuki289@gmail.com>
2019-11-14 11:28:45 +0700

    design navbar sama login

--- djojo-sehat/navbar.php
+++ djojo-sehat/navbar.php
@@ -69,9 +69,8 @@
     height: 50px;
     outline: none;
     border: none;
-    background: crimson;
-    color: white;
-    text-shadow: 0 0 10px crimson;
+    background: white;
+    color: black;
     padding: 0 80px 0 20px;
     border-radius: 30px;
     box-shadow: 0 0 25px 0 crimson,
@@ -135,54 +134,54 @@
   <div class="search-box"></div>
 </div>
         <li>
-          <a href='<?php echo $base_url ?>index.html'>
-            <span class='glyphicon glyphicon-home' aria-hidden='true'></span> Home
+          <a style="color:white;" href='<?php echo $base_url ?>index.html'>
+            <span class='glyphicon glyphicon-home' style='color:white;' aria-hidden='true'></span> Home
           </a>
         </li>
         <li>
-          <a href='<?php echo $base_url ?>katalog.html'>
+          <a style="color:white;" href='<?php echo $base_url ?>katalog.html'>
             <span class='glyphicon glyphicon-book' aria-hidden='true'></span> Katalog
           </a>
         </li>
         <li>
-          <a href='<?php echo $base_url ?>keranjang.html'>
+          <a style="color:white;" href='<?php echo $base_url ?>keranjang.html'>
             <span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'></span> Keranjang
           </a>
         </li>
         <li>
-          <a href='<?php echo $base_url ?>konsultasi.html'>
+          <a style="color:white;" href='<?php echo $base_url ?>konsultasi.html'>
             <span class='glyphicon glyphicon-comment' aria-hidden='true'></span> Konsultasi
           </a>
         </li>
         <li>
-          <a href='<?php echo $base_url ?>konfirmasi.html'>
+          <a style="color:white;" href='<?php echo $base_url ?>konfirmasi.html'>
             <span class='glyphicon glyphicon-bullhorn' aria-hidden='true'></span> Konfirmasi
           </a>
         </li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+          <a style="color:white;" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
             aria-expanded="false">
             <span class='glyphicon glyphicon-blackboard' aria-hidden='true'></span> Tentang Kami<span
               class="caret"></span>
           </a>
-          <ul class="dropdown-menu">
+          <ul class="dropdown-menu"style="background-color:black;">
             <li>
-              <a href='<?php echo $base_url ?>page/cara_order.html'>
+              <a style="color:white;" href='<?php echo $base_url ?>page/cara_order.html'>
                 <span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'></span> Cara Order
               </a>
             </li>
             <li>
-              <a href='<?php echo $base_url ?>page/ketentuan.html'>
+              <a style="color:white;" href='<?php echo $base_url ?>page/ketentuan.html'>
                 <span class='glyphicon glyphicon-flag' aria-hidden='true'></span> Ketentuan Belanja
               </a>
             </li>
             <li>
-              <a href='<?php echo $base_url ?>page/kontak.html'>
+              <a style="color:white;" href='<?php echo $base_url ?>page/kontak.html'>
                 <span class='glyphicon glyphicon-phone' aria-hidden='true'></span> Kontak
               </a>
             </li>
             <li>
-              <a href='<?php echo $base_url ?>page/profil.html'>
+              <a style="color:white;" href='<?php echo $base_url ?>page/profil.html'>
                 <span class='glyphicon glyphicon-user' aria-hidden='true'></span> Profil
               </a>
             </li>
@@ -210,7 +209,7 @@
       }
         else
         {
-          echo "<button type='button' class='btn btn-primary ml-5' style ='margin-top:7px;margin-left:10px;' data-toggle='modal' data-target='#login_modal'><span class='glyphicon glyphicon-user' aria-hidden='true'></span>
+          echo "<button type='button' class='btn btn-primary ml-5' style ='margin-top:7px;margin-left:10px;border:none;background-color:transparent;' data-toggle='modal' data-target='#login_modal'><span class='glyphicon glyphicon-user' aria-hidden='true'></span>
           Login
         </button>";
         }
