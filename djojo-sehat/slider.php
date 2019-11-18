7f5b303e711a458b38a86c9959bff7ac8dc43894
Alvin <asunayuuki289@gmail.com>
2019-11-18 16:43:01 +0700

    slider

--- djojo-sehat/slider.php
+++ djojo-sehat/slider.php
@@ -1,5 +1,6 @@
+
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner" style="width:400px;height:400px;">
+      <div class="carousel-inner" style="width:1150px;height:600px;">
         <?php
         //membuat query
         $sql = mysqli_query($koneksi,"SELECT * FROM tb_slider ORDER BY id_slider ASC");
@@ -7,7 +8,7 @@
         {
           echo '
           <div class="item '; if($row['active'] == 1){ echo 'active'; } echo '">
-            <img class="slide-image" src="'.$base_url.'images/slider/'.$row['gambar'].'" title="'.$row['judul_slider'].'" alt="'.$row['judul_slider'].'" width="100%"/>
+            <img class="img-fluid" alt="Cinque Terre" src="'.$base_url.'images/slider/'.$row['gambar'].'" title="'.$row['judul_slider'].'" alt="'.$row['judul_slider'].'" width="100%"/>
             <div class="carousel-caption">
               <h1 class="carousel-caption-header"></h1>
               <p class="carousel-caption-text hidden-sm hidden-xs"></p>
