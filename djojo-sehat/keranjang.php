11f35fdc4b0dd8499aff49b27b41b360c151c566
E41181803 - Lambang Arinanda Hadi <55471760+xattack404@users.noreply.github.com>
2019-11-20 09:27:20 +0700

    ja

--- djojo-sehat/keranjang.php
+++ djojo-sehat/keranjang.php
@@ -1,6 +1,6 @@
 
 <?php session_start();                    // Memulai session
-include 'config/koneksi.php';                     // Panggil koneksi ke database
+include 'config/koneksi.php';             // Panggil koneksi ke database
 include 'fungsi/base_url.php';            // Panggil fungsi base_url
 include 'fungsi/cek_session_public.php';  // Panggil fungsi cek session public
 include 'fungsi/tgl_indo.php';            // Panggil fungsi tanggal indonesia
