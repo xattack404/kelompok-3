aa6a1ae30894c8e6f03ecd959ab7fb39d48c7774
Alvin <asunayuuki289@gmail.com>
2019-12-09 11:35:13 +0700

    histori trans awal

--- djojo-sehat/admin/index.php
+++ djojo-sehat/admin/index.php
@@ -20,12 +20,19 @@ if(isset($_SESSION['username']))
   <link rel="shortcut icon" href="../images/fav.ico" />
 </head>
 
-<body class="login-page">
+<body class="login-page" style="background: rgba(76,76,76,1);
+background: -moz-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(89,89,89,1) 12%, rgba(102,102,102,1) 25%, rgba(71,71,71,1) 39%, rgba(44,44,44,1) 50%, rgba(0,0,0,1) 51%, rgba(17,17,17,1) 60%, rgba(43,43,43,1) 76%, rgba(28,28,28,1) 91%, rgba(19,19,19,1) 100%);
+background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(76,76,76,1)), color-stop(12%, rgba(89,89,89,1)), color-stop(25%, rgba(102,102,102,1)), color-stop(39%, rgba(71,71,71,1)), color-stop(50%, rgba(44,44,44,1)), color-stop(51%, rgba(0,0,0,1)), color-stop(60%, rgba(17,17,17,1)), color-stop(76%, rgba(43,43,43,1)), color-stop(91%, rgba(28,28,28,1)), color-stop(100%, rgba(19,19,19,1)));
+background: -webkit-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(89,89,89,1) 12%, rgba(102,102,102,1) 25%, rgba(71,71,71,1) 39%, rgba(44,44,44,1) 50%, rgba(0,0,0,1) 51%, rgba(17,17,17,1) 60%, rgba(43,43,43,1) 76%, rgba(28,28,28,1) 91%, rgba(19,19,19,1) 100%);
+background: -o-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(89,89,89,1) 12%, rgba(102,102,102,1) 25%, rgba(71,71,71,1) 39%, rgba(44,44,44,1) 50%, rgba(0,0,0,1) 51%, rgba(17,17,17,1) 60%, rgba(43,43,43,1) 76%, rgba(28,28,28,1) 91%, rgba(19,19,19,1) 100%);
+background: -ms-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(89,89,89,1) 12%, rgba(102,102,102,1) 25%, rgba(71,71,71,1) 39%, rgba(44,44,44,1) 50%, rgba(0,0,0,1) 51%, rgba(17,17,17,1) 60%, rgba(43,43,43,1) 76%, rgba(28,28,28,1) 91%, rgba(19,19,19,1) 100%);
+background: linear-gradient(135deg, rgba(76,76,76,1) 0%, rgba(89,89,89,1) 12%, rgba(102,102,102,1) 25%, rgba(71,71,71,1) 39%, rgba(44,44,44,1) 50%, rgba(0,0,0,1) 51%, rgba(17,17,17,1) 60%, rgba(43,43,43,1) 76%, rgba(28,28,28,1) 91%, rgba(19,19,19,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#4c4c4c', endColorstr='#131313', GradientType=1 );">
   <div class="login-box">
     <div align="center">
-      <h1><b>Admin Area</b></h1>
+      <h1><b style="color:white;">Admin Area</b></h1>
     </div>
-    <div class="login-box-body">
+    <div class="login-box-body" style="border-radius:20px;background-color:grey;">
       <div class="user image" align="center"><img src="../images/admin.png"><br /><br /></div>
 
       <form action="login.php" method="post">
@@ -38,8 +45,8 @@ if(isset($_SESSION['username']))
           <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
-          <div class="col-xs-4">
-            <button type="submit" name="submit" class="btn btn-primary btn-block btn-flat">Login</button>
+          <div class="col-xs-4" >
+            <button type="submit" name="submit" style ="border-radius:15px;width:320px;background-color:grey;border-color:grey;"  class="btn btn-primary btn-block btn-flat">Login</button>
           </div><!-- /.col -->
         </div>
       </form>
