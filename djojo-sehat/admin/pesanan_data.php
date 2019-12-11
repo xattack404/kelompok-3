51c6121a0d54147a6fe7b602bb9e22854edf1af7
E41181803 - Lambang Arinanda Hadi <55471760+xattack404@users.noreply.github.com>
2019-12-11 08:50:46 +0700

    sdoijf

--- djojo-sehat/admin/pesanan_data.php
+++ djojo-sehat/admin/pesanan_data.php
@@ -14,13 +14,14 @@
       </thead>
       <tbody>
       <?php
-      $sql = "SELECT trans_jual.id_trans, trans_jual.status, trans_jual.bukti_bayar, tb_member.id_member,
-                     tb_member.nama, tb_member.no_hp,tb_member.alamat,tb_status.id_status,tb_status.status_pesanan
+      $sql = "SELECT trans_jual.id_trans, trans_jual.status, trans_jual.bukti_bayar, 
+                      tb_member.id_member, tb_member.nama, tb_member.no_hp,tb_member.alamat,
+                      tb_status.id_status,tb_status.status_pesanan
       FROM trans_jual
       JOIN tb_member ON tb_member.id_member = trans_jual.id_member
       JOIN tb_status ON tb_status.id_status = trans_jual.status
 
-      WHERE trans_jual.status = 2 ";
+      ORDER BY trans_jual.status = 2 ";
 
       $result = mysqli_query($koneksi, $sql);
       if (mysqli_num_rows($result) > 0)
@@ -35,9 +36,15 @@
             <td style='text-align: center'>".$data['no_hp']."</td>
             <td style='text-align: center'><img src='../images/slider/".$data['bukti_bayar']."' width='100px' height='50px'></td>
             <td style='text-align: center'>".$data['status_pesanan']."</td>
-            <td style='text-align: center'>
+            <td style='text-align: left'>
               <a href='pesanan_detail.php?id_trans=$data[id_trans]'><button type='submit' class='btn btn-success'>Detail</button></a>
             </td>
+            <td style='text-align: left'>
+            <a href='pesanan_tolak.php?id_trans=$data[id_trans]'><button type='submit' class='btn btn-danger'>Tolak</button></a>
+          </td>
+          <td style='text-align: left'>
+            <a href='pesanan_proses.php?id_trans=$data[id_trans]'><button type='submit' class='btn btn-warning'>Proses</button></a>
+          </td>
           </tr>";
         }
       }
---
+++ djojo-sehat/admin/pesanan_tolak.php
@@ -0,0 +1,17 @@
+<?php session_start();
+include '../config/koneksi.php';                  // Panggil koneksi ke database
+include 'cek_login.php';                         // Panggil fungsi cek sudah login/belum
+include '../fungsi/cek_aksi_hapus.php';        	// Panggil fungsi boleh hapus data atau tidak
+
+$no_id   = mysqli_real_escape_string($koneksi, $_GET['id_trans']);
+
+$sql = "UPDATE trans_jual SET status=1 where id_trans='$no_id' ";
+if (mysqli_query($koneksi, $sql)) 
+{
+  echo "<script>alert('Update data berhasil! Klik ok untuk melanjutkan');location.replace('pesanan.php')</script>"; 
+}
+  else 
+  {
+      echo "Error updating record: " . mysqli_error($koneksi);
+  }
+?>
