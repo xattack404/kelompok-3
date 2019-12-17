669b74e2e562257bcac31168b5ecdea30330a5f1
E41181803 - Lambang Arinanda Hadi <55471760+xattack404@users.noreply.github.com>
2019-12-17 10:01:00 +0700

    z

--- djojo-sehat/admin/pesanan_tolak.php
+++ djojo-sehat/admin/pesanan_tolak.php
@@ -8,21 +8,20 @@ $no_id   = mysqli_real_escape_string($koneksi, $_GET['id_trans']);
 $sql = "UPDATE trans_jual SET status=1 where id_trans='$no_id' ";
 if (mysqli_query($koneksi, $sql)) 
 {
-  
+  //membuat fungsi cek data pada tabel detail jual bedasar ID TRANS
   $cek =mysqli_query($koneksi, "SELECT * FROM detail_jual where id_trans='$no_id'");
-
-
+  //mengaktifkan perulangan WHILE untuk mengambil semua data di tabel secara berulang kali
   while($row = mysqli_fetch_array($cek)){
     $id_barang= $row['id_barang'];
+  //membuat fungsi cek data tb barang bedasar ID barang yang ada di tabel detail jual
       $cek_stok =mysqli_query($koneksi, "SELECT * FROM tb_barang Where id_barang='$id_barang'");
       $hasil = mysqli_fetch_array($cek_stok);
       $stok = $hasil['jumlah'];
-
+  //fungsi mengembalikan stok ke awal karena batal transaksi
       $jumlah_asal = $stok + $row['jumlah'];
-
+ //fungsi perbarui stok lama dengan yang baru
       $query2 = $query2 = "UPDATE tb_barang SET jumlah ='$jumlah_asal' WHERE id_barang = '$id_barang'";
 
-      //Kurangi Stok pada tabel barang, bedasarkan id barang di keranjang
       mysqli_query($koneksi, $query2);
   }
       echo "<script>alert('Update data berhasil! Klik ok untuk melanjutkan');location.replace('pesanan.php')</script>"; 
