5cc08e86c177cb5b198759a849e587ad7e726f01
Alvin-kor <55471978+Alvin-kor@users.noreply.github.com>
2020-02-04 16:40:39 +0700

    Udah

--- djojo-sehat/profile.php
+++ djojo-sehat/profile.php
@@ -1,222 +1,273 @@
 <?php session_start();
 include 'fungsi/cek_session_public.php';
 include 'config/koneksi.php';
+include 'fungsi/base_url.php';
 $sql = mysqli_query($koneksi,"SELECT * from tb_member where id_member = $sesen_id");
+$sql2 = mysqli_query($koneksi,"SELECT * FROM tb_alamat where id_member = $sesen_id");
 $row = mysqli_fetch_array($sql);
-
+$row2 = mysqli_fetch_array($sql2);
+include 'navbar.php';
 ?>
-<div class="container">
-	<div class="topnav">
-	  <a  class="active" href="coba.php">biodata diri</a>
-	  <a href="profile_alamat.php">Daftar Alamat</a>
-	  <a href="notifikasi.php">Notifikasi</a>
-	  <a href="keamanan.php">Keamanan</a>
-	</div>
+<script src="<?php echo $base_url ?>template/js/jquery-3.4.1.min.js"></script>
 
-	<div style="padding-left:2px;padding-top: 45px; " >
-		<div id="ubah_semua">
-		<div class="container">
-		<div class=" offset-3 col-lg-6">
-		<form action="profile_proses.php" method="post">
-		<input type="hidden" name="id" value="<?= $sesen_id ?>">
-		<div class="row">
-			<p style="font-size: 15px"><b>Ubah Biodata Diri</b></p>
-		</div>
-		<div class="form-group">
-			<label for="nama">Nama</label>
-			<input type="text" value="<?= $sesen_nama ?>" name="nama" autofocus class="form-control" required>
-		</div>
-		<div class="form-group">
-			<label for="jenis_kelamin">Jenis Kelamin</label><br>
-			<input type="radio" name="jenis_kelamin" value="Laki-laki">Laki-laki
-			<input type="radio" name="jenis_kelamin" value="Perempuan">Perempuan
-		</div>
-		<div class="row">
-			<p style="font-size: 15px"><b>Ubah Kontak</b></p>
-		</div>
-		<div class="form-group">
-			<label for="email">Email</label>
-			<input type="text" name="email" value="<?= $row['email'] ?>" class="form-control" required>
-		</div>
-		<div class="form-group">
-			<label for="no_hp`">No_Hp</label>
-			<input type="number" name="no_hp"  value="<?= $row['no_hp'] ?>" class="form-control" required>	
-		</div>
-        <div class="form-group">
-        	<input type="submit" name="simpan_semua" value="simpan" class="btn btn-success">
-        </div>
-	</form>
-	</div>
-	</div>	
-	</div>
-	<div id="biodata">
-	  <div class="col-lg-5" style="background-color: #f1f1f1;border: 1px solid #c2d6d6;">
-	  		<div>
-	  			<br><br>
-	  		</div>
-	  	<div class="thumbnail" >
-            <div class="col-md-12" >
-              <img class="img-thumbnail" src="images/produk/coba.png" style="height: 250px;">
-            </div>
-            <br><br>
-            <div class="caption-full" >
-              <div class="row text-center" >
-              <div class="col-lg-12">
-	              <label class="form-control" ><b>Pilih Foto</b>
-	              <input id="files" style="visibility:hidden;" type="file"></label>
-              </div>
-              </div>
-              <p style="font-size: 10px;">Besar file: maksimum 1.000.000 bytes (10 Megabytes)
-				Ekstensi file yang diperbolehkan: .JPG .JPEG .PNG</p>
-            </div>
-          </div><br>
-          <div class="text-center"><label class="form-control"><b><i class="fa fa-lock" style="font-size: 18px"></i></b>&nbsp;Buat kata sandi</label></div>
-		</div>
-	  	<!-- <img class="center" src="images/produk/coba.png"> -->
-	  
-	  <div class="col-lg-6">
-	  
-		<div class="container"><div class="row">
-			<p style="font-size: 15px"><b>Ubah Biodata Diri</b></p>
-		</div>
-		<div class="row">
-			<div class="col-lg-6 ">
-				<p>Nama</p>
-			</div>
-			<div class="col-lg-6">
-				<?php if ($row['nama']=='') {?>
-					<input id="btn-nama" type="button" class="btn btn-primary btn-xs" value="tambah data"  onclick="nama()">
-					<div id="nama">
-						<form action="profile_proses.php" method="post">
-							<input type="hidden" name="id" value="<?= $sesen_id ?>">
-							<input type="text" class="form-control" name="nama" required placeholder="isi nama anda">
-							<button type="submit" name="simpan_nama" class="btn btn-success btn-xs">Simpan</button>
-						</form>
-					</div>
-				<?php }else{ ?>
-				<p><?= $row['nama'] ?></p>
-			<?php } ?>
-			</div>
-		</div><br>
-		<div class="row">
-			<div class="col-lg-6">
-				<p>Jenis Kelamin</p>
-			</div>
-			<div class="col-lg-6">
-				<?php if ($row['jenis_kelamin']=='') {?>
-					<input id="btn-jk" type="button" class="btn btn-primary btn-xs" value="tambah data"  onclick="jk()">
-					<div id="jk">
-						<form action="profile_proses.php" method="post">
-							<input type="hidden" name="id" value="<?= $sesen_id ?>">
-							<input type="radio" name="jenis_kelamin" value="Laki-laki">Laki-laki
-							<input type="radio" name="jenis_kelamin" value="Perempuan">
-							<button type="submit" name="simpan_jk" class="btn btn-success btn-xs">Simpan</button>
-						</form>
-					</div>
-				<?php }else{ ?>
-				<p><?= $row['jenis_kelamin'] ?></p>
-			<?php } ?>
-			</div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="row">
-			<p style="font-size: 15px"><b>Ubah Kontak</b></p>
-		</div>
-		<div class="row">
-			<div class="col-lg-6 ">
-				<p>Email</p>
-			</div>
-			<div class="col-lg-6">
-				<?php if ($row['email']=='') {?>
-					<input id="btn-email" type="button" class="btn btn-primary btn-xs" value="tambah data"  onclick="email()">
-					<div id="email">
-						<form action="profile_proses.php" method="post">
-							<input type="hidden" name="id" value="<?= $sesen_id ?>">
-							<input type="text" class="form-control" name="email">
-							<button type="submit" name="simpan_email" class="btn btn-success btn-xs">Simpan</button>
-						</form>
-					</div>
-				<?php }else{ ?>
-				<p><?= $row['email'] ?></p>
-			<?php } ?>
-			</div>
-		</div><br>
-		<div class="row">
-			<div class="col-lg-6 ">
-				<p>Nomor HP</p>
-			</div>
-			<div class="col-lg-6">
-				<?php if ($row['no_hp']=='') {?>
-					<input id="btn-no_hp" type="button" class="btn btn-primary btn-xs" value="tambah data"  onclick="no_hp()">
-					<div id="no_hp">
-						<form action="profile_proses.php" method="post">
-							<input type="hidden" name="id" value="<?= $sesen_id ?>">
-							<input type="number" class="form-control" name="no_hp" >
-							<button type="submit" name="simpan_no_hp" class="btn btn-success btn-xs">Simpan</button>
-						</form>
-					</div>
-				<?php }else{ ?>
-				<p><?= $row['no_hp'] ?></p>
-			<?php } ?>
-			</div>
-		</div><br>
-	</div><br><br><br>
-	<div class="text-center">
-	<input type="button" id="btn-all" onclick="ubahall()" class="btn btn-secondary" value="Ubah Semua Data">
-	</div>
-	<div class="container peringatan">
-		
-	</div>
-	</div>
+ <script src="<?php echo $base_url ?>template/js/jquery.js"></script>
+  <script src="<?php echo $base_url ?>template/js/bootstrap.min.js"></script>
+  <script src="<?php echo $base_url ?>js/jquery-3.3.1.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/jquery.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/popper.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/bootstrap.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/jquery.easing.1.3.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/jquery.waypoints.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/jquery.stellar.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/owl.carousel.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/aos.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/jquery.animateNumber.min.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/bootstrap-datepicker.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="<?= $base_url ?>template/Design/js/google-map.js"></script>
+  <script src="<?= $base_url ?>template/Design/js/main.js"></script>
+<link rel="stylesheet" href="<?php echo $base_url ?>template/Register/css/style.css"/>
 	
-	</div><!-- tutubiodata  -->
 	
-	</div>
-	</div>
+<section class="forms-section">
+  <div class="forms">
+    <div class="form-wrapper is-active">
+      <button type="button" class="switcher switcher-login" style="color:black">
+        Biodata Diri
+        <span class="underline"></span>
+      </button>
+      <form class="form form-login" enctype="multipart/form-data" action="profile_proses.php" method="post">
+        <fieldset>
+          <legend>Please, enter your email and password for login.</legend>
+          <div class="input-block">
+          	<input id="email" name="id" type="text" hidden value="<?= $sesen_id?>">
+            <label for="login-nama">Nama</label><br>
+            <label id="nama" style="color:black" for="login-nama"><?= $sesen_nama ?></label>
+            <div style="display: none" id="edit-nama">
+            <input id="nama" name="nama" type="text" required value="<?= $sesen_nama?>">
+          </div></div>
+          <div class="input-block">
+          	<label for="email">E-mail</label> <br>
+          	<label style="color: black" id="email1" for="login-email"><?= $row['email'] ?></label>
+             <div id="edit-email" style="display: none"n>
+            <input id="email" name="email" type="email" value="<?= $row['email']?>">
+            <p class="email" style="color: red"></p>
+           </div></div>
+           <div class="input-block">
+          	<label for="Jenis Kelamin">No Handphone</label> <br>
+          	<label style="color: black" id="no-hp" for="login-email"><?= $row2['no_hp']; if	(empty($row2['no_hp'])){echo "Data Belum Ada";}else{echo "$row2[no_hp]";}?></label>
+             <div id="edit-no-hp" style="display: none">
+            <input id="email-login" name="no-hp" type="number" value="<?= $row2['no_hp']?>">
+            <p class="no-hp" style="color: red"></p>
+           </div></div>
+          <div class="container" style="width:250;height: 250">
+  			<div class="card" style="width:250;height: 250">
+     			<div class="card__image-container" style="width:250;height: 250">
+       				<img id="upload" class="card__image" style="width:250;height: 250;border: solid black;border-radius: 5%;padding: 2px;margin-left: -15px" src="
 
-<style>
-	/*img{
-		height: 100px;
-		padding: 10px 10px 10px 10px;
-	}*/
-	.topnav {
-  overflow: hidden;
-  background-color: #f1f1f1;
-  border-bottom-color: red;
-}
-td{
-	margin-right: 10px;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 13px;
-  border-bottom: 3px solid transparent;
-}
-
-.topnav a:hover {
-  border-bottom: 3px solid green;
-}
-
-.topnav a.active {
-  border-bottom :3px solid skyblue;
+					<?php 
+					error_reporting(0);
+						if (empty($row[foto])){
+							echo "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2126&q=80";
+						}else{
+						echo"$base_url"."images/profile/"."$row[foto]";	
+						}
+					?>
 
 
-}
-.container p{
-	font-size: 12px;
-}
-</style>
+       				" alt="">
+       				<input id="files" id="gbr" name="gbr" style="display:none;margin-top: -250px;width: 250px;height: 250px" type="file">
+    			</div>
+			</div>
+		  </div>
+        </fieldset>
+        <button class="btn btn-primary py-3 px-4" style="width:150px;margin-left: 50px;margin-top: 15px;margin-bottom: -5px" type="button"id="Edit">Edit</button>
+        <button type="submit" id="submit" name="simpan_semua" class="btn-login" style="display: none">Submit</button>
+        <script>
+        		function event(){
+        			$('#files').trigger('click');
+        		}
+        	$('#Edit').on("click",function(){
+        		$('#nama').css("display","none");
+        		$('#email1').css("display","none");
+        		$('#no-hp').css("display","none");
+        		$('#Edit').css("display","none");
+        		$('#edit-nama').css("display","block");
+        		$('#edit-email').css("display","block");
+        		$('#edit-no-hp').css("display","block");
+        		$('#submit').css("display","block");
+        		$('#batal').css("display","block");
+        		$('#upload').on("click",event);
+        	});
+        	$('#batal').on("click",function(){
+        		$('#nama').css("display","block");
+        		$('#email1').css("display","block");
+        		$('#no-hp').css("display","block");
+        		$('#Edit').css("display","block");
+        		$('#edit-nama').css("display","none");
+        		$('#edit-email').css("display","none");
+        		$('#edit-no-hp').css("display","none");
+        		$('#submit').css("display","none");
+        		$('#batal').css("display","none");
+        		$('#upload').off("click",event)	;
+        	});
+        </script>
+      </form>
+    </div>
+    <div class="form-wrapper">
+      <button type="button" class="switcher switcher-signup" style="color: black">
+        Daftar Alamat
+        <span class="underline"></span>
+      </button>
+      <form class="form form-signup" action="profile_proses.php" method="post" style="width:400px">
+      	<p><button type="button" id="pilih" name="active" class="btn btn-primary py-3 px-4" style="width:150px;margin-left: 20px;margin-top: 15px;margin-bottom: -5px">Pilih Alamat</button>
+        	<button type="button" name="daftar" id="daftar" class="btn btn-primary py-3 px-4" style="width:150px;margin-left: 20px;margin-top: 15px;margin-bottom: -5px">Tambah Alamat</button></p>
+        </form>
+        <fieldset id="daftar_alamat" style="display: none">
+      <form class="form form-signup" action="profile_proses.php" method="post" style="width:400px">
+        	<input id="id" name="id" type="text" hidden value="<?= $sesen_id?>">
+          <legend>Please, enter your email, password and password confirmation for sign up.</legend>
+          <div class="input-block">
+            <label for="signup-email">Judul Alamat</label>
+            <input type="text" name="judul" id="judul" required>
+          </div>
+          <div class="input-block">
+            <label for="signup-email">Alamat</label>
+            <input type="text" name="alamat" id="alamat" required>
+          </div>
+          <div class="input-block">
+            <label for="signup-email">Provinsi</label><div class="select-wrap">
+                    <select name="prov" id="prov" class="form-control">
+                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <option value="">Provinsi</option>
+                      <?php
+                      $query = "SELECT * FROM prov ";
+                      $sql = mysqli_query($koneksi, $query);
+                      while($data = mysqli_fetch_array($sql)){
+                      echo '<option value="'.$data['id_prov'].'">'.$data['nama_prov'].'</option>';
+                      }
+                      ?>
+                    </select>
+                  </div>
+          </div>
+          <div class="input-block">
+            <label for="signup-email">Kabupaten</label>
+            <div class="select-wrap">
+                    <select name="kot" id="kot" class="form-control">
+                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <option value="">Kabupaten / Kota</option>
+                    </select>
+                  </div>
+          </div>
+          <div class="input-block">
+            <label for="signup-email">Kecamatan</label><div class="select-wrap">
+                    <select name="kec" id="kec" class="form-control">
+                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <option value="">Kecamatan</option>
+                     </select>
+                  </div>
+          </div>
+          <div class="input-block">
+            <label for="signup-email">Kode Pos</label>
+            <input type="number" name="kd_pos" id="kd_pos" required>
+          </div>
+          <div class="input-block">
+            <label for="signup-email">No Hp</label>
+            <input type="number" name="no_hp" id="no_hp" required>
+          </div>
+        <button type="submit" name="daftar_alamat_baru" class="btn-signup">Continue</button>
+        </fieldset>
+      </form>
+        <fieldset id="ubah" style="display: none">
+        	<form class="form form-signup" action="profile_proses.php" method="post">
+        	<input id="id" name="id" type="text" hidden value="<?= $sesen_id?>">
+        	<div class="input-block">
+            <label for="signup-email">Pilih Alamat</label>
+            <div class="select-wrap">
+                    <select name="amt" id="amt" class="form-control">
+                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <option value="">Pilih Alamat</option>
+                      <?php
+                      $query = "SELECT * FROM tb_alamat where id_member = $sesen_id";
+                      $sql = mysqli_query($koneksi, $query);
+                      while($data = mysqli_fetch_array($sql)){
+                      echo '<option value="'.$data['id_member'].'">'.$data['alamat'].'</option>';
+                      }
+                      ?>
+                    </select>
+                  </div>
+          </div>
+          <button type="submit" name="pilih_alamat" class="btn-signup">Set Active</button>
+          <button type="submit" name="hapus_alamat" class="btn-signup">Hapus</button>
+        </form>
+        </fieldset>
+    </div>
+  </div>
+</section>
+<script>
+const switchers = [...document.querySelectorAll('.switcher')]
 
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
 
+</script>
+<script type="text/javascript">
+    $(document).ready(function(){
+      //apabila terjadi event onchange terhadap object <select id=propinsi>
+      $("#prov").change(function(){
+        var prov = $("#prov").val();
+        $.ajax({
+            url: "fungsi/ambilkota.php",
+            data: "prov="+prov,
+            cache: false,
+            success: function(msg){
+                //jika data sukses diambil dari server kita tampilkan
+                //di <select id=kota>
+                $("#kot").html(msg);
+            }
+        });
+      });
+      $("#kot").change(function(){
+        var kot = $("#kot").val();
+        $.ajax({
+            url: "fungsi/ambilkecamatan.php",
+            data: "kot="+kot,
+            cache: false,
+            success: function(msg){
+                $("#kec").html(msg);
+            }
+        });
+      });
+      $(':input:not([type="submit"])').each(function() {
+          $(this).focus(function() {
+          $(this).addClass('hilite');
+          }).blur(function() {
+          $(this).removeClass('hilite');});
+        });
+      }); 
 
-<?php
-include "fungsi/imgpreview.php"; // Preview gambar yang akan diupload
-//include "../fungsi/tinymce.php";    // Editor teks Tinymce + Ajax File/ Photo Manager
-?>
+      $(document).ready(function(){
+        $('#nama').on('keyup',function(){
+          console.log($(this).val());
+        });
+      });
+
+    </script>
+    <script>
+    	$('#daftar').click(function(){
+    		$('#daftar_alamat').css("display","block");
+    		$('#ubah').css("display","none");
+    	});
+    	$('#pilih').click(function(){
+    		$('#ubah').css("display","block");
+    		$('#daftar_alamat').css("display","none");
+    	});
+    </script>
+  
--- djojo-sehat/profile_proses.php
+++ djojo-sehat/profile_proses.php
@@ -2,7 +2,7 @@
 
 include 'config/koneksi.php';
 include 'fungsi/base_url.php';
-
+include 'fungsi/cek_session_public.php';
 
 if (isset($_POST['simpan_nama'])) {
 	$id = mysqli_real_escape_string($koneksi,$_POST['id']);
@@ -51,20 +51,75 @@ if (isset($_POST['simpan_no_hp'])) {
 if (isset($_POST['simpan_semua'])) {
 	$id = mysqli_real_escape_string($koneksi,$_POST['id']);
 	$nama = mysqli_real_escape_string($koneksi,$_POST['nama']);
-	$jenis_kelamin = mysqli_real_escape_string($koneksi,$_POST['jenis_kelamin']);
+
+	$file_name = $_FILES['gbr']['name'];
+	$tmp = explode('.',$file_name);
+	$file_ext     = strtolower(end($tmp));
+	$file_tmp = $_FILES['gbr']['tmp_name'];
+	$lokasi = 'images/profile/'.$nama.'.'.$file_ext;
+	$img = $nama.'.'.$file_ext;
+
 	$email = mysqli_real_escape_string($koneksi,$_POST['email']);
+
+		move_uploaded_file($file_tmp, $lokasi);
+		$sql_semua = "UPDATE tb_member SET nama ='$nama', email ='$email',foto = '$img' WHERE id_member= '$id'";
+			if (mysqli_query($koneksi, $sql_semua)) 
+			{
+			 echo "<script>alert('biodata  berhasil diubah! Klik ok untuk melanjutkan');</script>";
+			}else{
+					echo "<script>alert('gagal diubah! Klik ok untuk melanjutkan');</script>";
+				 }	
+		
+}
+
+
+if  (isset($_POST['daftar_alamat_baru'])){
+	$id = mysqli_real_escape_string($koneksi,$_POST['id']);
+	$judul = mysqli_real_escape_string($koneksi,$_POST['judul']);
+	$alamat = mysqli_real_escape_string($koneksi,$_POST['alamat']);
+	$prov = mysqli_real_escape_string($koneksi,$_POST['prov']);
+	$kot = mysqli_real_escape_string($koneksi,$_POST['kot']);
+	$kec = mysqli_real_escape_string($koneksi,$_POST['kec']);
+	$kd_pos = mysqli_real_escape_string($koneksi,$_POST['kd_pos']);
 	$no_hp = mysqli_real_escape_string($koneksi,$_POST['no_hp']);
+	$sql = mysqli_query($koneksi,"INSERT INTO tb_alamat(	id_member,
+									judul_alamat,
+									alamat,
+									kecamatan,
+									kabupaten_kota,
+									provinsi,
+									kode_pos,
+									no_hp,
+									aktif)
+						   values(	'$id',
+									'$judul',
+									'$alamat',
+									'$prov',
+									'$kot',
+									'$kec',
+									'$kd_pos',
+									'$no_hp',
+									'1')");
+										echo "<script>alert('Berhasil Menambahkan Alamat Baru ! Klik ok untuk melanjutkan');</script>";
+										echo("Error description: " . mysqli_error($koneksi));	
+			
+}
 
-	
-		$sql_semua = "UPDATE tb_member SET nama ='$nama',  jenis_kelamin ='$jenis_kelamin', email ='$email', no_hp ='$no_hp' WHERE id_member= '$id'";
-		if (mysqli_query($koneksi, $sql_semua)) {
-			 echo "<script>alert('biodata  berhasil diubah! Klik ok untuk melanjutkan');location.replace('coba.php')</script>";
-		}else{
-			echo "<script>alert('gagal diubah! Klik ok untuk melanjutkan');location.replace('coba.php')</script>";
-		}
-	}
-
-
-
+if  (isset($_POST['pilih_alamat'])){
+	$id = mysqli_real_escape_string($koneksi,$_POST['id']);
+	$judul = mysqli_real_escape_string($koneksi,$_POST['amt']);
+	$sql = mysqli_query($koneksi,"UPDATE tb_alamat set aktif = '0' where alamat='$judul'");
+										echo "<script>alert('Berhasil Update Alamat Baru ! Klik ok untuk melanjutkan');</script>";
+										echo("Error description: " . mysqli_error($koneksi));	
+			
+}
+if  (isset($_POST['hapus_alamat'])){
+	$id = mysqli_real_escape_string($koneksi,$_POST['id']);
+	$judul = mysqli_real_escape_string($koneksi,$_POST['amt']);
+	$sql = mysqli_query($koneksi,"DELETE * from tb_alamat where alamat='$judul'");
+										echo "<script>alert('Berhasil Menghapus Alamat Baru ! Klik ok untuk melanjutkan');</script>";
+										echo("Error description: " . mysqli_error($koneksi));	
+			
+}
 
  ?>
