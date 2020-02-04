e8ba81daa50def77bcb5abe447bbe1658709ae16
Alvin-kor <55471978+Alvin-kor@users.noreply.github.com>
2020-02-04 23:50:42 +0700

    histori

--- djojo-sehat/histori_transaksi.php
+++ djojo-sehat/histori_transaksi.php
@@ -192,32 +192,32 @@ sup, .small {
     <?php
     $ambil =  mysqli_query($koneksi," SELECT tb_barang.id_barang,tb_barang.nama_barang,tb_barang.judul,
                                           tb_barang.berat,tb_barang.harga_jual,tb_member.id_member,
-                                          tb_member.nama,tb_member.alamat,tb_member.kecamatan,
-                                          tb_member.kabupaten_kota,tb_member.provinsi,tb_member.kode_pos,
-                                          tb_member.no_hp,tb_pengiriman.metode_pengiriman,detail_pengiriman.no_resi_pengiriman,
+                                          tb_member.nama,tb_alamat.id_member,tb_alamat.alamat,tb_alamat.kecamatan,
+                                          tb_alamat.kabupaten_kota,tb_alamat.provinsi,tb_alamat.kode_pos,
+                                          tb_alamat.no_hp,tb_pengiriman.metode_pengiriman,detail_pengiriman.no_resi_pengiriman,
                                           trans_jual.id_trans,trans_jual.id_member,trans_jual.status,trans_jual.total_bayar,
                                           tb_status.status_pesanan,
                                           kec.nama_kec,
                                           kabkot.nama_kabkot,kabkot.jne_reg,
                                           prov.nama_prov
               FROM trans_jual
+              LEFT JOIN tb_alamat ON tb_alamat.id_member = trans_jual.id_member
               LEFT JOIN tb_pengiriman ON tb_pengiriman.id_pengiriman = trans_jual.id_pengiriman
               LEFT JOIN detail_pengiriman ON detail_pengiriman.id_trans = trans_jual.id_trans
               LEFT JOIN tb_status ON tb_status.id_status = trans_jual.status
               LEFT JOIN tb_barang ON tb_barang.id_barang  = trans_jual.id_barang
               LEFT JOIN tb_member ON tb_member.id_member  = trans_jual.id_member
-              LEFT JOIN kec ON kec.id_kec = tb_member.kecamatan
+              LEFT JOIN kec ON kec.id_kec = tb_alamat.kecamatan
               LEFT JOIN kabkot ON kabkot.id_kabkot = kec.id_kabkot 
-                        AND kabkot.id_kabkot = tb_member.kabupaten_kota
-              LEFT JOIN prov ON prov.id_prov = kabkot.id_prov AND prov.id_prov = tb_member.provinsi
+                        AND kabkot.id_kabkot = tb_alamat.kabupaten_kota
+              LEFT JOIN prov ON prov.id_prov = kabkot.id_prov AND prov.id_prov = tb_alamat.provinsi
                 AND trans_jual.id_member= '$sesen_id' WHERE trans_jual.id_member= '$sesen_id'");
-
 if(mysqli_num_rows($ambil) > 0)
 {
     while($hasil        = mysqli_fetch_array($ambil)){
     ?>
 <script>
-    var id_trans = <?= $hasil['id_trans']?>;
+    var id_trans = <?= $hasil[id_trans]?>;
     var nama = "<?= $hasil[nama]?>";
     var total_bayar = <?= number_format($hasil['total_bayar'], 0, ',', '.').'';  ?>;
     var metode_pembayaran = "<?= $hasil[metode_pengiriman] ?>";
